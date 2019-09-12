725f58696ad4427a62315472d80aa4bceeafaab1
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-12 23:47:56 +0200

    alioth: Enabled updateable APEX
    Change-Id: Iae46cbf48ae5fbfa100e83e8d6ce19f225102cf3

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi

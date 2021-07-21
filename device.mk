0a1780273c08e77aeb2fec6b8de9f98df5a3fcc5
SahilSonar <sss.sonar2003@gmail.com>
2021-07-21 12:28:12 +0200

    alioth: Initial device tree
    Change-Id: I6e667c7ccd633593931e503cf10ca23ee9a8f713

---
+++ device.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2021 The Potato Open Sauce Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# A/B
+TARGET_IS_VAB := true
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Inherit from sm8250-common
+$(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/alioth/alioth-vendor.mk)

LineageOS/android_device_xiaomi_alioth

alioth: Add libinit for variants handling
Change-Id: Ia990f08d046192a42c0d951a2ce8d06b8794f6f0

BoardConfig.mk
@@ -34,6 +34,10 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_alioth
+TARGET_RECOVERY_DEVICE_MODULES := libinit_alioth
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
 

PotatoDevices/device_xiaomi_alioth

Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

BoardConfig.mk
@@ -17,6 +17,10 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_alioth
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_alioth
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
 
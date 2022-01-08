455d92e3eefeb05e05af7e3c8564c4559d224557
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-01-08 15:02:29 +0900

    alioth: drop duplicate/empty targets
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -26,8 +26,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
 
-include build/make/target/product/iorap_large_memory_config.mk
-
 # Enable VAB compression
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
@@ -152,7 +150,6 @@ PRODUCT_PACKAGES += \
     update_verifier
 
 PRODUCT_PACKAGES += \
-    linker.vendor_ramdisk \
     tune2fs.vendor_ramdisk \
     resize2fs.vendor_ramdisk
 
@@ -198,4 +195,3 @@ PRODUCT_COPY_FILES += \
 BOARD_BUILD_VENDOR_RAMDISK_IMAGE := true
 
 $(call inherit-product, vendor/xiaomi/alioth/alioth-vendor.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)

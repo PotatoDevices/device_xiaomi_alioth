e0fa86c854f6273c7d7a812239bac50ced8dac28
SahilSonar <sss.sonar2003@gmail.com>
2022-02-08 12:24:13 +0100

    alioth: Update build fingerprint to SQ1A.220205.002

--- potato_alioth.mk
+++ potato_alioth.mk
@@ -57,4 +57,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V12.5.7.0.RKHINXM release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.220205.002/8010174:user/release-keys

8984f0f465235be0b0ee19ac1b748186ddd9f5e3
Daniel Rosenberg <drosen@google.com>
2022-03-12 19:19:56 +0900

    alioth: Enable Project ID Quota support on userdata.
     * These features enable wide inodes on userdata, which
       is required for project ID quotas. Wide inodes can
       only be set at MKFS time.
    
     * This also enables casefolding, which is required for the
       removal of SDCardFS.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes.
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -32,6 +32,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Project ID Quota
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 TARGET_PRODUCT_PROP := $(LOCAL_PATH)/product.prop
 
 # Optimize everything for preopt

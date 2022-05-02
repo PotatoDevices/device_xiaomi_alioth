9abdc226b94e6ed923e041728600ccc52923ff49
SebaUbuntu <barezzisebastiano@gmail.com>
2022-05-02 12:07:35 +0200

    alioth: Build NFC stack

--- device.mk
+++ device.mk
@@ -189,6 +189,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
 
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc_snxxx@1.2-service \
+    com.android.nfc_extras \
+    libchrome.vendor \
+    NfcNci \
+    SecureElement \
+    Tag \
+    android.hardware.secure_element@1.2.vendor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)

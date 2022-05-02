30ce1739759ae5c52a778214cdbcb75f658d5eb7
SebaUbuntu <barezzisebastiano@gmail.com>
2022-05-02 11:57:53 +0200

    alioth: Build bluetooth libs

--- device.mk
+++ device.mk
@@ -103,6 +103,12 @@ PRODUCT_PACKAGES += \
     libvisualizer \
     libvolumelistener
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd

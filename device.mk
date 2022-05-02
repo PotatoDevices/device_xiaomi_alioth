a8103291b446327ce1b19fbc45b5bda3bf4606ee
SahilSonar <sss.sonar2003@gmail.com>
2022-05-02 12:10:56 +0200

    alioth: Build telephony packages

--- device.mk
+++ device.mk
@@ -240,5 +240,26 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Telephony
+PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper.xml \
+    qti_telephony_hidl_wrapper_prd.xml \
+    qti-telephony-utils \
+    qti-telephony-utils-prd \
+    qti_telephony_utils.xml \
+    qti_telephony_utils_prd.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/alioth/alioth-vendor.mk)

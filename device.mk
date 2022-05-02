537e5918a9b430dd8e86509758d05f842bce4dac
dianlujitao <dianlujitao@lineageos.org>
2022-05-02 12:19:50 +0200

    alioth: Build dummy android.hidl.base@1.0

--- device.mk
+++ device.mk
@@ -168,6 +168,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \

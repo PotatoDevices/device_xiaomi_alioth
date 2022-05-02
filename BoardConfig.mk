76b997aeb70852ccbe7885e9d2cb823cdd35bdc2
SahilSonar <sss.sonar2003@gmail.com>
2022-05-02 10:33:06 +0200

    alioth: Add kernel flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,20 @@ TARGET_USES_GRALLOC4 := true
 TARGET_USES_HWC2 := true
 TARGET_USES_ION := true
 
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CONFIG := alioth_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 

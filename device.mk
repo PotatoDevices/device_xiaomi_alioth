0dd613a476a20b6a361e7b10a908df9770803f2f
SahilSonar <sss.sonar2003@gmail.com>
2022-05-02 12:21:58 +0200

    alioth: Build vndservicemanager

--- device.mk
+++ device.mk
@@ -298,6 +298,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.xiaomi_alioth

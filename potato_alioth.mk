4ee4d173de68b3153f9e472907b02986387ea57d
SahilSonar <sss.sonar2003@gmail.com>
2022-06-07 06:48:05 +0200

    alioth: Update build fingerprint to SQ3A.220605.009

--- potato_alioth.mk
+++ potato_alioth.mk
@@ -22,4 +22,4 @@ PRODUCT_MODEL := M2012K11AG
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := google/redfin/redfin:12/SP2A.220505.002/8353555:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:12/SQ3A.220605.009.A1/8643238:user/release-keys
